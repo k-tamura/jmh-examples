588ab4b15073422b0b564367d7f294cbd3c3f856
tamura <k-tamura>
2020-12-01 21:00:01 +0900

    Initial commit

---
+++ src/main/java/org/t246osslab/sample/jmh/BadBenchmark.java
@@ -0,0 +1,39 @@
+package org.t246osslab.sample.jmh;
+
+import java.io.IOException;
+import java.util.logging.*;
+
+public class BadBenchmark {
+    static Logger logger = Logger.getLogger(MyBenchmark.class.getName());
+
+    static {
+        try {
+            logger.setLevel(Level.INFO);
+            Handler handler = new FileHandler("test.log");
+            logger.addHandler(handler);
+            Formatter formatter = new SimpleFormatter();
+            handler.setFormatter(formatter);
+            logger.setUseParentHandlers(false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+
+        int MAX_LOOP_COUNT = 1000;
+        System.out.println("1000回実行した処理時間の比較（単位：ミリ秒）");
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i < MAX_LOOP_COUNT; i++) {
+            if (logger.isLoggable(Level.INFO))
+                logger.info("Logger is " + logger.getLevel() + " level.");
+        }
+        System.out.println("1つ目: " + (System.currentTimeMillis() - startTime));
+
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i < MAX_LOOP_COUNT; i++) {
+            logger.info("Logger is " + logger.getLevel() + " level.");
+        }
+        System.out.println("2つ目: " + (System.currentTimeMillis() - startTime));
+    }
+}
---
+++ src/main/java/org/t246osslab/sample/jmh/MyBenchmark.java
@@ -0,0 +1,35 @@
+package org.t246osslab.sample.jmh;
+
+import org.openjdk.jmh.annotations.Benchmark;
+
+import java.io.IOException;
+import java.util.logging.*;
+
+public class MyBenchmark {
+    static Logger logger = Logger.getLogger(MyBenchmark.class.getName());
+
+    static {
+        try {
+            logger.setLevel(Level.INFO);
+            Handler handler = new FileHandler("test.log");
+            logger.addHandler(handler);
+            Formatter formatter = new SimpleFormatter();
+            handler.setFormatter(formatter);
+            logger.setUseParentHandlers(false);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Benchmark
+    public void testMethod() {
+        if (logger.isLoggable(Level.INFO)) {
+            logger.info("Logger is " + Level.INFO + " level.");
+        }
+    }
+
+    @Benchmark
+    public void testMethod2() {
+        logger.info("Logger is " + Level.INFO + " level.");
+    }
+}
